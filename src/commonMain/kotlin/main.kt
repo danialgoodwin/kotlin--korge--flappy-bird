365c5ed657358d6f24497287f8bb0483f48a8713
Danial Goodwin <danialgoodwin@gmail.com>
2022-04-17 21:12:40 -0600

    Initial project

---
+++ src/commonMain/kotlin/main.kt
@@ -0,0 +1,12 @@
+import com.soywiz.klock.seconds
+import com.soywiz.korge.*
+import com.soywiz.korge.tween.*
+import com.soywiz.korge.view.*
+import com.soywiz.korim.color.Colors
+import com.soywiz.korim.format.*
+import com.soywiz.korio.file.std.*
+import com.soywiz.korma.geom.degrees
+import com.soywiz.korma.interpolation.Easing
+
+suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors.WHITE) {
+}
